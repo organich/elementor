1a521e905ca0455ccff8c40f69ffe6b35da127fd
svitlanadykun <svitlanad@elementor.red>
2024-09-09 12:23:28 +0300

    run tests on the latest version + wait for db update

--- tests/playwright/wp-authentication.ts
+++ tests/playwright/wp-authentication.ts
@@ -16,24 +16,18 @@ export async function login( apiRequest: APIRequest, user: string, password: str
 	return context;
 }
 
-export async function fetchNonce( context: APIRequestContext, baseUrl: string, page: Page ) {
-	const response = await context.get( `${ baseUrl }/wp-admin/post-new.php` );
+export async function fetchNonce( page: Page, baseUrl: string ) {
+	await page.goto( `${ baseUrl }/wp-admin/post-new.php` );
 
-	if ( ! response.ok() ) {
-		throw new Error( `
-				Failed to fetch nonce: ${ response.status() }.
-				${ await response.text() }
-				${ response.url() }
-			` );
-	}
-	let pageText = await response.text();
+	let pageText = await page.content();
+
 	if ( pageText.includes( 'WordPress has been updated! Next and final step is to update your database to the newest version' ) ) {
 		await page.waitForFunction(
 			() => ! document.body.innerText.includes( 'WordPress has been updated! Next and final step is to update your database to the newest version' ),
 			{ timeout: 60000 },
 		);
 		await page.reload();
-		pageText = await page.content(); // Fetch new content after reload
+		pageText = await page.content();
 	}
 	const nonceMatch = pageText.match( /var wpApiSettings = .*;/ );
 	if ( ! nonceMatch ) {
