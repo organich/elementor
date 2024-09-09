d8d34166a2d993c676ec89433d319b899010b7b6
svitlanadykun <svitlanad@elementor.red>
2024-09-09 14:47:36 +0300

    update db

--- tests/playwright/wp-authentication.ts
+++ tests/playwright/wp-authentication.ts
@@ -16,17 +16,37 @@ export async function login( apiRequest: APIRequest, user: string, password: str
 	return context;
 }
 
-export async function fetchNonce( context: APIRequestContext, baseUrl: string ) {
+export async function fetchNonce( context: APIRequestContext, baseUrl: string, { browser } ) {
 	const response = await context.get( `${ baseUrl }/wp-admin/post-new.php` );
 
 	if ( ! response.ok() ) {
 		throw new Error( `
-				Failed to fetch nonce: ${ response.status() }.
-				${ await response.text() }
-				${ response.url() }
-			` );
+            Failed to fetch nonce: ${ response.status }.
+            ${ await response.text() }
+            ${ response.url() }
+        ` );
 	}
-	const pageText = await response.text();
+
+	let pageText = await response.text();
+	if ( pageText.includes( 'WordPress has been updated! Next and final step is to update your database to the newest version' ) ) {
+		const contextUI = await browser.newContext();
+		const page = await contextUI.newPage();
+		page.goto( '/wp-admin/post-new.php' );
+		await page.getByText( 'Update WordPress Database' ).click();
+		await page.getByText( 'Continue' ).click();
+
+		const retryResponse = await context.get( `${ baseUrl }/wp-admin/post-new.php` );
+		if ( ! retryResponse.ok() ) {
+			throw new Error( `
+                Failed to fetch nonce after database update: ${ retryResponse.status }.
+                ${ await retryResponse.text() }
+                ${ retryResponse.url() }
+            ` );
+		}
+
+		pageText = await retryResponse.text();
+	}
+
 	const nonceMatch = pageText.match( /var wpApiSettings = .*;/ );
 	if ( ! nonceMatch ) {
 		throw new Error( `Nonce not found on the page:\n"${ pageText }"` );
